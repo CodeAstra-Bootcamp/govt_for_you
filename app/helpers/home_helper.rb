0f4ac8ea6e8e4867a6cb3b2d1f6908548fcc556c
Aryan Singh <aryan@iitgn.ac.in>
2015-07-10 01:40:35 +0530

    Changing the layout to sidebar layout

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,9 @@
 module HomeHelper
+  def sidebar_link(text, link, fa_class_name, badge_count = nil)
+    content_tag(:div, class: "link") do
+      content_tag(:span, '', class: "fa fa-#{fa_class_name}") +
+        content_tag(:span, badge_count, class: "badge") +
+        link_to(text, link)
+    end
+  end
 end
